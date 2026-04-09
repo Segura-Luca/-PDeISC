ba8f1aa0b8e694b842dab365a93a6a5686f89d70
Segura-Luca <134236522+Segura-Luca@users.noreply.github.com>
2026-04-09 19:43:35 -0300

    Update calculos.mjs

--- node/calculos.mjs
+++ node/calculos.mjs
@@ -1,5 +1,5 @@
-// calculos.mjs
-
+// 'export' permite que estas funciones se puedan usar en otros archivos mediante 'import'
+// Usamos funciones de flecha (=>), que son una forma corta de escribir funciones
 export const sumar = (a, b) => a + b;
 export const restar = (a, b) => a - b;
 export const multiplicar = (a, b) => a * b;
